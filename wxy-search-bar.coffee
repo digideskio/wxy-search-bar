1614e963852798c88f39a3b6a80a17564d79a573
Helen <xiaoyingw12@gmail.com>
2015-02-05 21:29:18 -0800

    initial

--- wxy-search-bar.coffee
+++ wxy-search-bar.coffee
@@ -1,3 +1,30 @@
 Polymer
+  open: false
+
   ready: ->
+    # HACK
+    @eventWaiting = false
+    document.addEventListener 'click', (e) =>
+      @eventWaiting = true
+      @async =>
+        if @eventWaiting
+          @open = false
+          @$.container.classList.toggle 'show', @open
+          return
+      , 0
+      return
+
+    return
+
+  onSearchTap: (e) ->
+    @open = !@open
+    @$.container.classList.toggle 'show', @open
+
+    if @open
+      @$.input.focus()
+
+    return
+
+  onContainerTap: (e) ->
+    @eventWaiting = false
     return
--- wxy-search-bar.js
+++ wxy-search-bar.js
@@ -1,6 +1,30 @@
 (function() {
   Polymer({
-    ready: function() {}
+    open: false,
+    ready: function() {
+      this.eventWaiting = false;
+      document.addEventListener('click', (function(_this) {
+        return function(e) {
+          _this.eventWaiting = true;
+          _this.async(function() {
+            if (_this.eventWaiting) {
+              _this.open = false;
+              _this.$.container.classList.toggle('show', _this.open);
+            }
+          }, 0);
+        };
+      })(this));
+    },
+    onSearchTap: function(e) {
+      this.open = !this.open;
+      this.$.container.classList.toggle('show', this.open);
+      if (this.open) {
+        this.$.input.focus();
+      }
+    },
+    onContainerTap: function(e) {
+      this.eventWaiting = false;
+    }
   });
 
 }).call(this);
